ebc54a254b9c3af141162dde1cfe19b0a7a67b3d
Blakeley Bond <burrick2003@yahoo.com>
2017-11-01 00:32:34 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+# input_array = any 1D array
+# i = the block passthrough value
+# output_array = the assembled output string, TO BE RETURNED
+
+
+def my_collect(input_array)
+  if block_given?
+    counter = 0
+    output = []
+    while counter < input_array.size
+
+    end
+    output
+  end
+      
+
+
+
+end
 
 
